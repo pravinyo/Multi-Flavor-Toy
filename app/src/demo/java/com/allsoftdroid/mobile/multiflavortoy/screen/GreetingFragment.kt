f526f67eec3910f1ab01b6520c2c7f0500485ccf
Pravinyo <pravinyo12@gmail.com>
2020-08-13 12:21:39 +0530

    fragment demo

--- app/src/demo/java/com/allsoftdroid/mobile/multiflavortoy/screen/GreetingFragment.kt
+++ app/src/demo/java/com/allsoftdroid/mobile/multiflavortoy/screen/GreetingFragment.kt
@@ -1,10 +1,30 @@
 package com.allsoftdroid.mobile.multiflavortoy.screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.allsoftdroid.mobile.multiflavortoy.R
 
-class GreetingFragment {
+
+class GreetingFragment : Fragment() {
     companion object {
         fun getGreetings(): String {
             return "This is Demo version"
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_greeting,container,false)
+        view.findViewById<TextView>(R.id.tv_greeting_message).apply {
+            text = getGreetings()
+        }
+        return view
     }
 }
