10b9a96a57eb907e00c809d540f509725fa38288
Pravinyo <pravinyo12@gmail.com>
2020-08-13 23:30:17 +0530

    loading fragment using manager

--- app/src/main/java/com/allsoftdroid/mobile/multiflavortoy/MainActivity.kt
+++ app/src/main/java/com/allsoftdroid/mobile/multiflavortoy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.allsoftdroid.mobile.multiflavortoy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.allsoftdroid.mobile.multiflavortoy.screen.GreetingFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainer,GreetingFragment(),"greeting")
+            .commit()
     }
 }
