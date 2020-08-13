47db18952f384c3cf8fb8483029818952b310379
Pravinyo <pravinyo12@gmail.com>
2020-08-13 12:21:55 +0530

    fragment full

--- app/src/full/java/com/allsoftdroid/mobile/multiflavortoy/screen/GreetingFragment.kt
+++ app/src/full/java/com/allsoftdroid/mobile/multiflavortoy/screen/GreetingFragment.kt
@@ -1,9 +1,29 @@
 package com.allsoftdroid.mobile.multiflavortoy.screen
 
-class GreetingFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.allsoftdroid.mobile.multiflavortoy.R
+
+class GreetingFragment : Fragment() {
     companion object {
         fun getGreetings(): String {
             return "This is Full version"
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_greeting,container,false)
+        view.findViewById<TextView>(R.id.tv_greeting_message).apply {
+            text = getGreetings()
+        }
+        return view
     }
 }
