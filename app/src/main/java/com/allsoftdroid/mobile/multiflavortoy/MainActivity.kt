c80d6f06fa56f3b18219f35fbb5356f9dcaed2bc
Pravinyo <pravinyo12@gmail.com>
2020-08-13 12:22:25 +0530

    clean code

--- app/src/main/java/com/allsoftdroid/mobile/multiflavortoy/MainActivity.kt
+++ app/src/main/java/com/allsoftdroid/mobile/multiflavortoy/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.allsoftdroid.mobile.multiflavortoy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.allsoftdroid.mobile.multiflavortoy.screen.GreetingFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        findViewById<TextView>(R.id.text).apply {
-            text = GreetingFragment.getGreetings()
-        }
     }
 }
